d7b332374626f4b0971832ba1a28dd165b8416a0
sandydasan <santidasan441@gmail.com>
2023-07-13 13:02:04 +0530

    simple-interest-calculater

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
+import TextField from '@mui/material/TextField';
 import './App.css';
+import { Button } from '@mui/material';
+import { useState } from 'react';
+
 
 function App() {
+  //state creation
+  const [principle,setPrinciple]=useState('')
+  const [year,setYear]=useState('')
+  const [rate,setRate]=useState('')
+  const [interest,setInterest]=useState('')
+  console.log(principle);
+  console.log(rate);
+  console.log(year);
+
+  const handleCalculate=(e)=>{
+     let output = principle*year*rate /100
+     console.log(output);
+     setInterest(output)
+  }
+
+  const reset=(e)=>{
+    setInterest(0)
+    setPrinciple(0)
+    setRate(0)
+    setYear(0)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div className="container">
+        <div className="head">
+         <h1 >Simple Interest Calculator</h1>
+         <p>The rate at which you borrow or lend money is called the simple interest. If a borrower takes money from a lender, an extra amount of money is paid back to the lender. The borrowed money which is given for a specific period is called the principal. The extra amount which is paid back to the lender for using the money is called the interest.</p>
+         </div>
+         <div className="card">
+          <h3 className='symbol'>&#8377; {interest}</h3>
+          <p id='para'>Your Total Simple Interest</p>
+
+         
+         </div>
+       
+         <div className='input'>
+         <TextField id="outlined-basic" label="Principle Amount" variant="outlined"
+         onChange={e=>setPrinciple(e.target.value)} value={principle}/>
+         <br />
+         <TextField id="outlined-basic" label="Rate of Interest" variant="outlined" 
+         onChange={e=>setRate(e.target.value)} value={rate}/>
+         <br/>
+         <TextField id="outlined-basic" label="Year" variant="outlined"
+         onChange={e=>setYear(e.target.value)}  value={year}/>
+         </div>
+         <Button onClick={handleCalculate} id='btn1' variant="contained">Calculate</Button>&nbsp;&nbsp;
+          <Button onClick={reset} id='btn2' variant="outlined">Reset</Button>
+       </div>
       </header>
     </div>
   );
